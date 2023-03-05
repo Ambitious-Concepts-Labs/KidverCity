af1e5813ff92956678ec9d2501c2a299d17b2734
Dominique Hosea <dominique11h@yahoo.com>
2023-03-04 23:33:02 -0600

    Remove build unused errors

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import {
 	CarouselProvider,
 	Slider,
-	Slide,
+	// Slide,
 	ButtonBack,
 	ButtonNext,
 } from "pure-react-carousel";
